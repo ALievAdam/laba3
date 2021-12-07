172642024e67e7333fe6e5d91d60671b6a23b4e3
ALievAdam <90550031+ALievAdam@users.noreply.github.com>
2021-12-07 14:07:30 +0500

    доработал

--- laba3/laba3/laba3.cpp
+++ laba3/laba3/laba3.cpp
@@ -1,39 +1,50 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
 #include<Windows.h>
-#include<string>
+#include <string>
+#include<conio.h>
 
-using namespace std;
+using std::string;
+using std::vector;
 
+using dict = std::unordered_map<char, char>; // Использование несортируемого словаря
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int i = 0, k;
-    char ABC[] =
+    string s;
+    int k;
+    vector<char> result; // Создание вектора для вывода результата
+    string alphabet("абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
+    dict alphabet_map; // Перевод строки алфавита в словарь
+    std::cout << "Введите сообщение на русском языке строчными буквами, которое будет закодировано: " << std::endl;
+    std::getline(std::cin, s); // Ввод текста
+    std::cout << "\nВведите сдвиг:" << std::endl;
+    std::cin >> k; // Ввод сдвига
+    int n = alphabet.size(); // Подсчет количества символов в алфавите
+    k = ((k % n) + n) % n; // Условие, для шага больше чем количество символов в алфавите и для декодировки (отрицательного шага)
+    for (int i = 0; i < n; i++) // Цикл проходящий по каждому введенному символу
     {
-                'а','б','в','г','д','е','ё','ж','з','и',
-                'й','к','л','м','н','о','п','р','с',
-                'т','у','ф','х','ц','ч','ш','щ','ъ',
-                'ы','ь','э','ю','я' 
-    };
-    char msg[150];
-    cout << "Введите сообщение на русском языке строчными буквами, которое будет закодировано: " << endl;
-    cin.getline(msg, 150);
-    cout << "\nВведите сдвиг:" << endl;
-    cin >> k;
-    cout << endl;
-    char* begin = ABC;
-    char* end = ABC + sizeof(ABC);
-    for (; msg[i]; ++i)
-    {
-        char* ch = find(begin, end, msg[i]);
-        if (ch != end) msg[i] = *(begin + (ch - begin + k) % sizeof(ABC));
+        auto ind = (i + k) % n; // Считает индекс символа со сдвигом
+        alphabet_map[alphabet[i]] = alphabet[ind]; // Присваивание символу значения со здвигом
     }
-    cout << "Полученное закодированное сообщение: " << endl;
-    cout << msg << endl;
+    std::cout << "\nПолученное закодированное сообщение:" << std::endl;
+    for (auto it : s) {
+        try {
+            result.push_back(alphabet_map.at(it)); // Вставка изменённого символа в конечный результат 
+        }
+        catch (const std::out_of_range& exception) // Обработка исчключений для пробела и других символов. Возвращает то же, что и ввели
+        {
+            result.push_back(it);
+        }
+    }
+    for (auto it : result) // Вывод результата
+    {
+        std::cout << it;
+    }
+    _getch();
     return 0;
 }
 
